cdc3e35a9d36b150eb03bbc95defc330822739f8
EnricoSpanier <149034926+EnricoSpanier@users.noreply.github.com>
2024-11-10 20:11:33 -0300

    Update main.java

--- método_da_divisão_e_linear_probing/main.java
+++ método_da_divisão_e_linear_probing/main.java
@@ -1,3 +1,30 @@
-// Enrico Minto Spainer Tia:10419775
-// Guilherme Vecchi  Tia:1048517
+/*
+Enrico Minto Spainer Tia:10419775
+Guilherme Vecchi  Tia:1048517
+
+*/
+import java.util.Scanner;
+public class Main{
+	 public static void main(String[] args) {
+        HashTable tabelaHash = new HashTable();
+        Scanner sc = new Scanner(System.in);
+
+        // Inserir 50 valores na tabela hash
+        for (int i = 1; i <= 51; i++) {
+            System.out.println("insira o "+i+" valor:");
+            int valor = sc.nextInt();
+            tabelaHash.inserirValor(valor);
+        }
+
+        // Exibir a tabela hash gerada
+        tabelaHash.exibirTabela();
+
+        // Tentar encontrar alguns elementos na tabela
+        int[] elementosParaEncontrar = {5, 500, 999, 50}; // Exemplos de elementos a serem encontrados
+        for (int elemento : elementosParaEncontrar) {
+            boolean encontrado = tabelaHash.encontrarValor(elemento);
+            System.out.println("Valor " + elemento + " encontrado: " + encontrado);
+        }
+    }
+}
 
